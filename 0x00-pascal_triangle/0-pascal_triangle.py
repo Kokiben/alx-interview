a9d00e14660ebd47fbc6ade8295a1f40b048b077
Kokiben <bennassarkaoutar@gmail.com>
2024-10-03 23:57:37 +0000

    pascaltriangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize the first row of the triangle
+
+    for a in range(1, n):
+        row = [1]  # Every row starts with a 1
+        for b in range(1, a):
+            # Each element is the sum of the two elements above it
+            row.append(triangle[a-1][b-1] + triangle[a-1][b])
+        row.append(1)  # Every row ends with a 1
+        triangle.append(row)
+
+    return triangle
