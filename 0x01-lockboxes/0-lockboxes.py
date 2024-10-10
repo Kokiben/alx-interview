1c75f8ec3b9cff83b8a8ce8f0dd099029f8277ed
Kokiben <bennassarkaoutar@gmail.com>
2024-10-10 18:36:04 +0000

    boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""
+You have n locked boxes, numbered from 0 to n - 1, 
+each potentially containing keys to other boxes.
+"""
 
 def canUnlockAll(boxes):
-    # Total number of boxes
-    n = len(boxes)
-    # A set to keep track of unlocked boxes
-    unlocked = set([0])
-    # A list to act as a queue for boxes to be processed
-    keys = [0]
+    """
+    Determines if all the boxes can be opened.
 
-    # While there are keys to process
-    while keys:
-        # Take the first key
-        current_key = keys.pop()
-        # Iterate through keys in the current box
-        for key in boxes[current_key]:
-            # If the key unlocks a new box, add it to the unlocked set and to the keys list
-            if key not in unlocked and key < n:
-                unlocked.add(key)
-                keys.append(key)
+    :param boxes: List of lists where each sublist contains keys to other boxes.
+    :return: True if all boxes can be unlocked, otherwise False.
+    """
+    if not boxes or type(boxes) is not list:
+        return False
 
-    # Return True if all boxes have been unlocked, else False
-    return len(unlocked) == n
+    # Initialize a list of accessible boxes starting with box 0.
+    accessible_boxes = [0]
+
+    # Iterate over each accessible box and use its keys to unlock more boxes.
+    for box_index in accessible_boxes:
+        for key in boxes[box_index]:
+            # If the key opens a box that hasn't been accessed yet, add it to the list.
+            if key not in accessible_boxes and key < len(boxes):
+                accessible_boxes.append(key)
+
+    # Check if the number of accessible boxes matches the total number of boxes.
+    return len(accessible_boxes) == len(boxes)
