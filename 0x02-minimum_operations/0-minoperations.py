a77738e17e284ac5d81607e1023cb4ac1e4e0ce4
Kokiben <bennassarkaoutar@gmail.com>
2024-10-17 22:33:59 +0000

    operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" In a text file, there is a single character H """
+
+def minOperations(target_count: int) -> int:
+    """
+    Calculatesmin num of opera needed to achieve exactly target_count H char.
+    """
+    # If target_count is less than 2, it's not feasible to create more char through opr.
+    if target_count < 2:
+        return 0
+
+    total_operations = 0
+    current_divisor = 2
+
+    # Loop through numbers starting from 2 to find factors of target_count.
+    while current_divisor <= target_count:
+        # If current_divisor is a factor of target_cou.
+        # Keep dividing target_count by current_divisor until factor.
+        while target_count % current_divisor == 0:
+            total_operations += current_divisor
+            target_count //= current_divisor
+        # Move on to the next potential divisor.
+        current_divisor += 1
+
+    return total_operations
