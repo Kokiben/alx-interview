0cb22d5d652a6e56cc71cbe0dae71ccfac605226
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-12-05 22:48:06 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,12 +6,19 @@ Island Perimeter
 
 def island_perimeter(grid):
     """
-     returns the perimeter of the island described in grid
-    :param grid:
-    :return:
+    Calculate the perimeter of the island described in grid.
+    The perimeter is determined by counting the transitions between 
+    land (1) and water (0) both horizontally and vertically.
+    Args:
+        grid (list of list of int): A rectangular grid where
+        0 represents water and 1 represents land.
+    Returns:
+        int: The perimeter of the island.
     """
-    area = 0
-    for row in grid + list(map(list, zip(*grid))):
-        for i1, i2 in zip([0] + row, row + [0]):
-            area += int(i1 != i2)
-    return area
+    perimeter = 0
+    # Combine rows and columns using `zip` for vertical traversal
+    for line in grid + list(map(list, zip(*grid))):
+        # Count transitions between land and water
+        for current_cell, next_cell in zip([0] + line, line + [0]):
+            perimeter += int(current_cell != next_cell)
+    return perimeter
