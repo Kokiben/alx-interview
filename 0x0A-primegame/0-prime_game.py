bb2d5a8d8db3a9d67438218f7ba94154100f199a
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-12-11 01:11:10 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-""" Prime Game Algorithm Python """
+""" prime game """
 
 
-def is_prime(n):
+def _IsPrime(n):
     """ Checks if a number given n is a prime number """
     for i in range(2, int(n ** 0.5) + 1):
         if not n % i:
@@ -16,7 +16,7 @@ def calculate_primes(n, primes):
     top_prime = primes[-1]
     if n > top_prime:
         for i in range(top_prime + 1, n + 1):
-            if is_prime(i):
+            if _IsPrime(i):
                 primes.append(i)
             else:
                 primes.append(0)
