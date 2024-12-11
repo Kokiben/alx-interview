f18896531ac99747a50fffe09f5e9cbd9cd7276b
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-12-11 01:57:40 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-Define isWineer function, a solution to the Prime Game problem
+Prime Game
 """
 
 
 def _IsPrime(n):
-    """Return list of prime numbers between 1 and n inclusive
-       Args:
-        n (int): upper boundary of range. lower boundary is always 1
+    """Check if a number n is prime.
     """
     prime = []
     sieve = [True] * (n + 1)
@@ -20,13 +18,7 @@ def _IsPrime(n):
 
 
 def isWinner(x, nums):
-    """
-    Determines winner of Prime Game
-    Args:
-        x (int): no. of rounds of game
-        nums (int): upper limit of range for each round
-    Return:
-        Name of winner (Maria or Ben) or None if winner cannot be found
+    """Determine the overall winner after x rounds of the game.
     """
     if x is None or nums is None or x == 0 or nums == []:
         return None
